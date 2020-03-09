c104c6a13182eb4924c802fb9e5fdaf344d12575
Tymoteusz Tula <tymoteusz.tula@gmail.com>
2020-03-09 12:29:13 +0000

    critical behaviour reproduced at higher transition point

--- BranchingAnihilatingRandomWalk.py
+++ BranchingAnihilatingRandomWalk.py
@@ -228,7 +228,7 @@ class BARandomWalk3:
                     (self.r_walkers[i_walker][1] >= self.N)):
                     self.r_walkers.pop(i_walker)
                     i_walker -= 1
-                elif (self.board[(self.r_walkers[i_walker][0], 
+                elif (self.board[(self.r_walkers[i_walker][0] , 
                             self.r_walkers[i_walker][1])] == 1):
                     self.r_walkers.pop(i_walker)
                     i_walker -= 1
@@ -259,32 +259,158 @@ class BARandomWalk3:
 
     def show_trace(self):
         return self.board
-                
+
+VEC_DOWN_RIGHT = np.array([-1, 1])
+VEC_DOWN_LEFT = np.array([-1, -1])
+VEC_UP_RIGHT = np.array([1, 1])
+VEC_UP_LEFT = np.array([1, -1])
 
 
+class BARandomWalk4:
+    def __init__(self, N, sigma):
+        self.prob_branch = sigma / (1 + sigma)
+        self.board = np.zeros((N,N))
+        self.r_walkers = []
+        self.r_walkers.append(np.random.randint(N, size=(2)))
+        self.N = N
 
+    def one_epoch_evolution(self):
+        i_walker = 0
+        new_walkers = []
         
+        while i_walker < len(self.r_walkers):
+            r_branch = np.random.rand()
+
+            if r_branch > self.prob_branch:
+                r_move = np.random.randint(8)
+                next_move = ((r_move == 0) * VEC_UP +
+                             (r_move == 1) * VEC_DOWN +
+                             (r_move == 2) * VEC_LEFT +
+                             (r_move == 3) * VEC_RIGHT +
+                             (r_move == 4) * VEC_DOWN_LEFT +
+                             (r_move == 5) * VEC_DOWN_RIGHT +
+                             (r_move == 6) * VEC_UP_LEFT +
+                             (r_move == 7) * VEC_UP_RIGHT)
+                self.board[(self.r_walkers[i_walker][0], 
+                            self.r_walkers[i_walker][1])] = 1
+                self.r_walkers[i_walker] += next_move
+
+                if ((self.r_walkers[i_walker][0] < 0) or
+                    (self.r_walkers[i_walker][0] >= self.N) or
+                    (self.r_walkers[i_walker][1] < 0) or
+                    (self.r_walkers[i_walker][1] >= self.N)):
+                    self.r_walkers.pop(i_walker)
+                    i_walker -= 1
+                elif (self.board[(self.r_walkers[i_walker][0] , 
+                            self.r_walkers[i_walker][1])] == 1):
+                    self.r_walkers.pop(i_walker)
+                    i_walker -= 1
+            else:
+                r_born = np.random.randint(8)
+                born_vector = ((r_born == 0) * VEC_UP +
+                             (r_born == 1) * VEC_DOWN +
+                             (r_born == 2) * VEC_LEFT +
+                             (r_born == 3) * VEC_RIGHT +
+                             (r_born == 4) * VEC_DOWN_LEFT +
+                             (r_born == 5) * VEC_DOWN_RIGHT +
+                             (r_born == 6) * VEC_UP_LEFT +
+                             (r_born == 7) * VEC_UP_RIGHT)
+                new_walker = np.copy(self.r_walkers[i_walker]) + born_vector
+
+                if ((new_walker[0] < 0) or
+                    (new_walker[0] >= self.N) or
+                    (new_walker[1] < 0) or
+                    (new_walker[1] >= self.N)):
+                    pass
+                elif (self.board[new_walker[0], new_walker[1]] == 1):
+                    pass
+                else:
+                    new_walkers.append(new_walker)
+            i_walker += 1
+            
+        self.r_walkers = self.r_walkers + new_walkers
+
+    def whole_evolution(self):
+        while self.r_walkers != []:
+            self.one_epoch_evolution()
+
+    def show_trace(self):
+        return self.board
+
+                
+def fnn_phase_transition(sigma_min = 0, sigma_max = 2,
+                         sigma_step = 0.01, av_no = 100,
+                         N = 100):
+    sigma = np.arange(sigma_min, sigma_max, sigma_step)
+    density = np.zeros(sigma.shape[0])
+    h = 0
+
+    for i_sigma in sigma:
+        for _ in range(av_no):
+            tempRandomWalk = BARandomWalk3(N, i_sigma)
+            tempRandomWalk.whole_evolution()
+            density[h] += np.sum(tempRandomWalk.show_trace())
+        density[h] /= av_no
+        h += 1
+        print("Progress: ", i_sigma * 100 / sigma_max, 
+              "% completed")
+
+    return sigma, density
+
+def snn_phase_transition(sigma_min = 0, sigma_max = 2,
+                         sigma_step = 0.01, av_no = 10,
+                         N = 100):
+    sigma = np.arange(sigma_min, sigma_max, sigma_step)
+    density = np.zeros(sigma.shape[0])
+    h = 0
+
+    for i_sigma in sigma:
+        for _ in range(av_no):
+            tempRandomWalk = BARandomWalk4(N, i_sigma)
+            tempRandomWalk.whole_evolution()
+            density[h] += np.sum(tempRandomWalk.show_trace())
+        density[h] /= av_no
+        h += 1
+        print("Progress: ", i_sigma * 100 / sigma_max, 
+              "% completed")
+
+    return sigma, density  
 
 
 
 # main code
 
-def main():
-    N = 100
-    sigma = 0.7
-    
-    randomWalkGenerator = BARandomWalk3(N, sigma)
-    randomWalkGenerator.whole_evolution()
-    
-    trace = randomWalkGenerator.show_trace()
-    
-    plt.figure(1)
-    plt.imshow(trace)
-    plt.show()
-    
-main()
+def main(case):
+
+    if case == 1:
+        N = 100
+        sigma = 0.94
         
+        randomWalkGenerator = BARandomWalk4(N, sigma)
+        randomWalkGenerator.whole_evolution()
         
+        trace = randomWalkGenerator.show_trace()
+        
+        plt.figure(1)
+        plt.imshow(trace)
+        plt.show()
+    elif case == 2:
+        sigma1, density1 = fnn_phase_transition()
+        sigma2, density2 = snn_phase_transition()
+
+        plt.figure(1)
+        plt.plot(sigma1, density1)
+        plt.title("Nearest neighbour phase transition")
+
+        plt.figure(2)
+        plt.plot(sigma2, density2)
+        plt.title("Second nearest neighbour phase transition")
+
+        plt.show()
+    
+main(2)
+        
+     
         
         
         
